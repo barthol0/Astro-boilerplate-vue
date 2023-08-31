e376d03698f297c5b4657dff32b71feb19656338
Bart Bialek <bart.bialek@micron-research.com>
2023-08-31 23:41:21 +0100

    Rewrite all components in Vue 3, single repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
@@ -10,13 +10,6 @@ export default defineConfig({
   // base: '.', // Set a path prefix.
   site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
   trailingSlash: 'always', // Use to always append '/' at end of url
-  // Important!
-  // Only official '@astrojs/*' integrations are currently supported by Astro.
-  // Add 'experimental.integrations: true' to make 'astro-robots-txt' working
-  // with 'astro build' command.
-  experimental: {
-    integrations: true,
-  },
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
@@ -25,10 +18,10 @@ export default defineConfig({
     },
   },
   integrations: [
-    react(),
-    tailwind({}),
+    vue(),
+    tailwind(),
     sitemap(),
     robotsTxt(),
-    astroImageTools,
+    astroImageTools
   ],
 });
---
+++ src/components/HeroSocial.vue
@@ -0,0 +1,15 @@
+<template>
+  <img
+    :src="src"
+    :alt="alt"
+    loading="lazy"
+    class="h-12 w-12 hover:translate-y-1"
+  />
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const src = ref("");
+const alt = ref("");
+</script>
